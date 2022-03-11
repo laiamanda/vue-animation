6560dbd7f1a227a6313f9cc2f2ddffb54f9c1f53
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-03-10 21:57:59 -0500

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,241 @@
+<template>
+    <router-view v-slot="slotProps">
+      <transition name = "route" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
+  
+  
+  <!--<div class="container">
+    <users-list></users-list>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <div class= "container">
+    <transition name="para" 
+        :css="false"
+        @before-enter="beforeEnter"
+        @enter="enter" 
+        @after-enter="afterEnter"
+        @before-leave="beforeLeave"
+        @leave="leave"
+        @after-leave="afterLeave"
+        @enter-cancelled="enterCancelledd"
+        @leave-cancelled="leaveCancelled"
+      >
+      <p v-if="paraIsVisible" >This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>-->
+</template>  
+
+<script>
+//import UsersList from './components/UsersList.vue';
+
+export default {
+  components: {
+    //UsersList,
+  },
+  data() {
+    return { 
+      animatedBlock: false,
+      dialogIsVisible: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null
+    };
+  },
+  methods: {
+    enterCancelledd(el){
+      console.log(el);
+      clearInterval(this.enterInteval);
+    },
+    leaveCancelled(el){
+      console.log(el);
+      clearInterval(this.leaveInteval);
+    },
+    beforeEnter(el){
+      console.log("beforeEnter");
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    enter(el, done){
+      console.log("enter");
+      let round = 1
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.1;
+        round++;
+        if (round > 10){
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter(el){
+      console.log('afterEnter');
+      console.log(el);
+    },
+    beforeLeave(el){
+      console.log("beforeLeave");
+      el.style.opacity = 1;
+    },
+    leave(el, done){
+      console.log('leave');
+      console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.1;
+        round++;
+        if (round > 10){
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave(){
+      console.log('after leave');
+    },
+    showUsers(){
+      this.usersAreVisible = true;
+    },
+    hideUsers(){
+      this.usersAreVisible = false;
+    },
+    animateBlock(){
+      this.animatedBlock = true;
+    },
+    toggleParagraph(){
+      this.paraIsVisible = !this.paraIsVisible;
+    },
+    showDialog() { 
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s ease-out; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+.animate{
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
+  }
+/*
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px);
+} 
+.para-enter-active {
+ transition: all 0.3s  ease-out; 
+  animation: slide-fade 0.3s ease-out;
+}
+.para-enter-to {
+ opacity: 1;
+  transform: translateY(0); 
+}
+
+.para-leave-from{
+ opacity: 1;
+  transform: translateY(0);
+}
+.para-leave-active{
+ transition: all 0.3s  ease-in; 
+  animation: slide-fade 0.3s ease-in;
+}
+.para-leave-to{
+ opacity: 0;
+  transform: translateY(30px);
+} */
+.fade-button-enter-from,
+.fade-button-leave-to{
+  opacity: 0;
+}
+.fade-button-enter-active{
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leve-active{
+  transition: opacity 0.3s ease-in
+}
+.fade-button-enter-to,
+.fade-button-leave-from{
+  opacity: 1;
+}
+.route-enter-from{}
+.route-enter-active{
+  animation: slide-scale 0.4s ease-out;
+}
+.route-enter-to{}
+.route-leave-to{}
+.route-leave-active{
+  animation: slide-scale 0.4s ease-in;
+}
+.route-leave-active{}
+@keyframes slide-fade{
+  0% {
+    transform: translateX(0px) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }  
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+</style>
